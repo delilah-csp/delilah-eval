d4fe49b85d0cbc0831cd3295c8a13622e290ee0c
Niclas Hedam <niclas@hed.am>
2023-03-13 12:00:41 +0100

    Add FS module to enable programs to mount and unmount drives

--- delilah.c
+++ delilah.c
@@ -10,6 +10,10 @@
 #define DATA_SIZE 1024 * 1024 * 128
 #define PROG_SIZE 1024 * 1024
 
+struct delilah_mount_t fs0 = {.nvme_handle = "nvme0n1p1",
+                              .mount_point = "/media/nvme0n1p1/",
+                              .fs_type = "ext4"};
+
 int delilah_write_prog(struct io_uring *ring, int fd, void *buf, size_t len) {
   struct io_uring_sqe *sqe;
   struct delilah_dma *dma;
@@ -104,8 +108,6 @@ int main() {
     return 3;
   }
 
-  sz = file_read(prog, PROG_SIZE, "programs/version.o");
-
   /* Initialize io_uring queue with four entries */
   p.flags = IORING_SETUP_SQE128;
   ret = io_uring_queue_init(4, &ring, p.flags);
@@ -115,22 +117,26 @@ int main() {
     return 2;
   }
 
+  // Write versioning program to Delilah
+  sz = file_read(prog, PROG_SIZE, "programs/version.o");
   delilah_write_prog(&ring, fd, prog, sz);
   io_uring_wait_cqe(&ring, &cqe);
   io_uring_cqe_seen(&ring, cqe);
 
+  // Execute versioning program on Delilah
   delilah_exec_prog(&ring, fd, 0, 0, 0);
   io_uring_wait_cqe(&ring, &cqe);
   io_uring_cqe_seen(&ring, cqe);
 
+  // Read versioning result from Delilah
   delilah_io(&ring, fd, dst, 64, 0);
   io_uring_wait_cqe(&ring, &cqe);
   io_uring_cqe_seen(&ring, cqe);
 
   printf("Delilah reported itself to be of version: %ld\n\n",
          *((uint64_t *)dst));
-  printf("Begin table with results: \n");
 
+  printf("Begin table with results: \n");
   printf("bytes, load, write, exec, read, v1, v2\n");
 
   uint64_t sizes[] = {100, 1000, 10000, 100000, 1000000, 10000000};
@@ -141,7 +147,7 @@ int main() {
   sz = file_read(prog, PROG_SIZE, "programs/file.o");
   for (int i = 0; i < 6; i++) {
     file->size = sizes[i] * 10; // the size denotes the number of strings
-    sprintf(file->filename, "/media/nvme/%ld.txt", sizes[i]);
+    sprintf(file->filename, "/run/media/nvme0n1p1/%ld.txt", sizes[i]);
 
     gettimeofday(&loading, NULL);
     delilah_write_prog(&ring, fd, prog, sz);
@@ -191,76 +197,5 @@ int main() {
     printf("%s, %s\n", first, last);
   }
 
-  sz = file_read(prog, PROG_SIZE, "programs/file_map.o");
-  for (int i = 0; i < 6; i++) {
-    file->size = sizes[i] * 10; // the size denotes the number of strings
-    switch (i) {
-    case 0:
-      sprintf(file->filename, "1kb");
-      break;
-    case 1:
-      sprintf(file->filename, "10kb");
-      break;
-    case 2:
-      sprintf(file->filename, "100kb");
-      break;
-    case 3:
-      sprintf(file->filename, "1mb");
-      break;
-    case 4:
-      sprintf(file->filename, "10mb");
-      break;
-    case 5:
-      sprintf(file->filename, "100mb");
-      break;
-    }
-
-    gettimeofday(&loading, NULL);
-    delilah_write_prog(&ring, fd, prog, sz);
-    io_uring_wait_cqe(&ring, &cqe);
-    io_uring_cqe_seen(&ring, cqe);
-
-    gettimeofday(&writeing, NULL);
-    delilah_io(&ring, fd, src, sizeof(struct delilah_file_t), 1);
-    io_uring_wait_cqe(&ring, &cqe);
-    io_uring_cqe_seen(&ring, cqe);
-
-    gettimeofday(&executing, NULL);
-    delilah_exec_prog(&ring, fd, 0, 0, 0);
-    io_uring_wait_cqe(&ring, &cqe);
-    io_uring_cqe_seen(&ring, cqe);
-
-    gettimeofday(&reading, NULL);
-    delilah_io(&ring, fd, dst, sizeof(struct delilah_file_t) + sizes[i] * 10,
-               0);
-    io_uring_wait_cqe(&ring, &cqe);
-    io_uring_cqe_seen(&ring, cqe);
-
-    gettimeofday(&done, NULL);
-
-    timersub(&writeing, &loading, &sub_load);
-    timersub(&executing, &writeing, &sub_write);
-    timersub(&reading, &executing, &sub_exec);
-    timersub(&done, &reading, &sub_read);
-
-    printf("%lu, ", sizes[i] * 10);
-    printf("%ld.%06ld, ", (long int)sub_load.tv_sec,
-           (long int)sub_load.tv_usec);
-    printf("%ld.%06ld, ", (long int)sub_write.tv_sec,
-           (long int)sub_write.tv_usec);
-    printf("%ld.%06ld, ", (long int)sub_exec.tv_sec,
-           (long int)sub_exec.tv_usec);
-    printf("%ld.%06ld, ", (long int)sub_read.tv_sec,
-           (long int)sub_read.tv_usec);
-
-    char first[10], last[10];
-    first[9] = '\0';
-    last[9] = '\0';
-
-    memcpy(first, dst + sizeof(struct delilah_file_t), 9);
-    memcpy(last, dst + sizeof(struct delilah_file_t) + (sizes[i] * 10) - 10, 9);
-
-    printf("%s, %s\n", first, last);
-  }
   return 0;
 }
--- delilah.h
+++ delilah.h
@@ -4,3 +4,9 @@ struct delilah_file_t {
   uint64_t size;
   char filename[128];
 };
+
+struct delilah_mount_t {
+  char nvme_handle[32];
+  char mount_point[128];
+  char fs_type[32];
+};
